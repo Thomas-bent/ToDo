e95bc8ffdfed1548a0b96e0f4c707075b92ced90
Thomas Bent <thomas.bentlohner@outlook.de>
2024-12-29 16:21:27 +0100

    ToDos and Todo View Added

---
+++ src/ToDo.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import { Label } from "./components/ui/label";
+import {Checkbox} from "./components/ui/checkbox.tsx";
+
+export interface ToDo {
+    title: string;
+    description: string;
+    tags: string[];
+    list: string;
+}
+
+const ToDoDisplay: React.FC<{todo: ToDo, className?: string}> = (props: {todo: ToDo, className?: string}) => {
+    return <div className={"flex items-center space-x-2 " + props.className}>
+        <Checkbox id={props.todo.title} />
+        <Label htmlFor={props.todo.title}>{props.todo.description}</Label>
+    </div>
+}
+
+export default ToDoDisplay;
---
+++ src/ToDoView.tsx
@@ -0,0 +1,38 @@
+﻿import {useState} from "react";
+import ToDoDisplay, {ToDo} from "./ToDo.tsx";
+import {Input} from "./components/ui/input.tsx";
+import {Button} from "./components/ui/button.tsx";
+
+const ToDoView = () => {
+
+    const [todos, setTodos] = useState<ToDo[]>([{
+        title: "test",
+        description: "testdescription",
+        tags: ["test"],
+        list: "testlist"
+    }])
+    const [newTodoValue, setNewTodoValue] = useState("");
+
+    const onHandleNewToDo = () => {
+        setTodos([...todos, {
+            title: newTodoValue,
+            description: newTodoValue,
+            tags: [],
+            list: "testlist"
+        }])
+        setNewTodoValue("")
+    }
+
+    return <div className="flow-root h-dvh mt-5 w-full">
+        {
+            todos.map((todo) => <ToDoDisplay className={"m-1"} todo={todo} />)
+        }
+        <div className="flex mt-5 w-1/5">
+            <Input onChange={e => setNewTodoValue(e.target.value)}
+                   value={newTodoValue} type={"text"} placeholder={"Add new Todo"} />
+            <Button onClick={onHandleNewToDo}>Add</Button>
+        </div>
+    </div>
+}
+
+export default ToDoView
